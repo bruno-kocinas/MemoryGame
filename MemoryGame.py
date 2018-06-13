b179bf7c047df92943562114adaf7fe12922d5a7
Bruno <brunokocinas@gmail.com>
2018-06-13 13:20:42 +0300

    Latest commit

--- MemoryGame.py
+++ MemoryGame.py
@@ -17,7 +17,7 @@ def index():
     return template('MemoryGame.html')
 
 def main():
-    run(host='localhost', port=8000)
+    run(host='localhost', port=7001)
 
 if __name__ == '__main__':
     main()
